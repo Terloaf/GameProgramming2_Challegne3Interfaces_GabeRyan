715bceba69ea59d8556d8637319f3d465717cb46
Terloaf <greenyryan2501@gmail.com>
2026-01-28 09:47:43 -0400

    Initial commit

---
+++ GameProgramming2_Challegne3Interfaces_GabeRyan/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProgramming2_Challegne3Interfaces_GabeRyan
+{
+    internal class Program
+    {
+    
+        public static bool _isPlaying = true;
+        static Player player = new Player(playerPosX: 5, playerPosY: 5, ConsoleColor.Blue);
+        static Enemy enemy = new Enemy(enemyPosX: 20, enemyPosY: 20, ConsoleColor.Red);
+        static void Main(string[] args)
+        {
+            
+            while(_isPlaying == true)
+            {
+
+                Draw();
+                ConsoleKeyInfo input = Console.ReadKey();
+                if(input == ConsoleKey.M)
+                {
+
+                }
+
+
+
+
+            }
+           
+            
+
+
+        }
+
+
+
+        public struct Position
+        {
+            public int _x;
+            public int _y;
+
+            public Position(int x, int y)
+            {
+                _x = x;
+                _y = y;
+            }
+        }
+
+        public class Player
+        {
+
+            ConsoleColor _playerColour;
+            public Position _playerPosition;
+
+            
+            public Player(int playerPosX, int playerPosY, ConsoleColor playerColour)
+            {
+                _playerPosition = new Position(playerPosX, playerPosY);
+                _playerColour = playerColour;
+            }
+            
+        }
+
+
+
+        public class Enemy
+        {
+            
+            ConsoleColor _enemyColour;
+            public Position _enemyPosition;
+
+            public Enemy(int enemyPosX, int enemyPosY, ConsoleColor enemyColour)
+            {
+                _enemyPosition = new Position(enemyPosX, enemyPosY);
+                _enemyColour = enemyColour;
+            }
+            enum MovementType
+            {
+                agressive,
+                passive,
+                random
+
+            };
+            MovementType _movementType;
+
+            public void Move()
+            {
+                if(_movementType == MovementType.agressive)
+                {
+                    if(_enemyPosition._x < player._playerPosition._x)
+                    {
+                        _enemyPosition._x += 1;
+                    }
+                    if (_enemyPosition._x > player._playerPosition._x)
+                    {
+                        _enemyPosition._x -= 1;
+                    }
+                    if (_enemyPosition._y < player._playerPosition._y)
+                    {
+                        _enemyPosition._y += 1;
+                    }
+                    if (_enemyPosition._y > player._playerPosition._y)
+                    {
+                        _enemyPosition._y -= 1;
+                    }
+                }
+                if(_movementType == MovementType.passive)
+                {
+                    if (_enemyPosition._x > player._playerPosition._x)
+                    {
+                        _enemyPosition._x += 1;
+                    }
+                    if (_enemyPosition._x < player._playerPosition._x)
+                    {
+                        _enemyPosition._x -= 1;
+                    }
+                    if (_enemyPosition._y > player._playerPosition._y)
+                    {
+                        _enemyPosition._y += 1;
+                    }
+                    if (_enemyPosition._y < player._playerPosition._y)
+                    {
+                        _enemyPosition._y -= 1;
+                    }
+                }
+                if(_movementType == MovementType.random)
+                {
+                    Random random = new Random();
+
+                    int randomX = random.Next(-1, 2);
+                    int randomY = random.Next(-1, 2);
+
+                    _enemyPosition._x += randomX;
+                    _enemyPosition._y += randomY;
+                }
+                
+                
+            }
+
+
+        }
+
+        public static void Draw()
+        {
+            Console.SetCursorPosition(player._playerPosition._x, player._playerPosition._y);
+            Console.Write("O");
+            Console.SetCursorPosition(enemy._enemyPosition._x, enemy._enemyPosition._y);
+            Console.Write("X");
+        }
+    }
+}
---
+++ GameProgramming2_Challegne3Interfaces_GabeRyan/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GameProgramming2_Challegne3Interfaces_GabeRyan")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NSCC")]
+[assembly: AssemblyProduct("GameProgramming2_Challegne3Interfaces_GabeRyan")]
+[assembly: AssemblyCopyright("Copyright © NSCC 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cc85b6fe-3e7b-4db9-8ee5-b0460536a79b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
